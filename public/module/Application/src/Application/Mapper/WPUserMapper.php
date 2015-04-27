449681a732ed7774bfebb91460f02cd9bbf9fa41
Kathryn Reeve <Kathryn@binaryKitten.com>
2015-04-27 23:23:48 +0100

    update to WPUser Model/Mapper to allow direct connection to meta data

--- public/module/Application/src/Application/Mapper/WPUserMapper.php
+++ public/module/Application/src/Application/Mapper/WPUserMapper.php
@@ -8,6 +8,8 @@
 
 namespace Application\Mapper;
 
+use Application\Model\WPUser;
+use Zend\Db\ResultSet\HydratingResultSet;
 use ZfcBase\Mapper\AbstractDbMapper;
 
 class WPUserMapper extends AbstractDbMapper
@@ -16,4 +18,35 @@ class WPUserMapper extends AbstractDbMapper
      * @var string
      */
     protected $tableName = 'wp_users';
+
+
+    /**
+     * @param $ID
+     * @return WPUser|bool|HydratingResultSet
+     */
+    public function getUser($ID)
+    {
+        return $this->getUserBy('ID', $ID);
+    }
+
+
+    /**
+     * @param $field
+     * @param $value
+     * @return bool|WPUser|HydratingResultSet
+     */
+    public function getUserBy($field, $value)
+    {
+        $where = [];
+        $where[$field] = $value;
+        $s = $this->getSelect()->columns(['*'])->where($where);
+        $result =  $this->select($s);
+        if ($result->count() == 0) {
+            return false;
+        } elseif ($result->count() == 1) {
+            return $result->current();
+        } else {
+            return $result;
+        }
+    }
 }
--- public/module/Application/src/Application/Model/WPUser.php
+++ public/module/Application/src/Application/Model/WPUser.php
@@ -9,7 +9,12 @@
 namespace Application\Model;
 
 
-class WPUser
+use Application\Mapper\WPUserMetaMapper;
+use Zend\Db\ResultSet\HydratingResultSet;
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class WPUser implements ServiceLocatorAwareInterface
 {
     public $ID;
     public $user_login;
@@ -20,4 +25,58 @@ class WPUser
     public $user_activation_key;
     public $user_status;
     public $display_name;
+
+    /** @var ServiceLocatorInterface $serviceLocator  */
+    protected $serviceLocator;
+
+    /** @var array $metaData */
+    protected $metaData = [];
+
+    /**
+     * Set service locator
+     *
+     * @param ServiceLocatorInterface $serviceLocator
+     *
+     * @return $this
+     */
+    public function setServiceLocator(ServiceLocatorInterface $serviceLocator)
+    {
+        $this->serviceLocator = $serviceLocator;
+        return $this;
+    }
+
+    /**
+     * Get service locator
+     *
+     * @return ServiceLocatorInterface
+     */
+    public function getServiceLocator()
+    {
+        return $this->serviceLocator;
+    }
+
+    function __get($name)
+    {
+        /** @var WPUserMetaMapper $wpMetaMapper */
+        $wpMetaMapper = $this->getServiceLocator()->get('Application\Mapper\WPUserMetaMapper');
+
+        if (empty($this->metaData)) {
+            $this->metaData = $wpMetaMapper->getMetaForUser($this);
+            if ($this->metaData instanceof HydratingResultSet) {
+                $this->metaData = $wpMetaMapper->metaArray($this->metaData);
+            }
+        }
+
+        if ($name == 'roles') {
+            $roles = $wpMetaMapper->maybe_unserialize($this->metaData['wp_capabilities']);
+            return array_keys($roles);
+        }
+
+        if (!isset($this->metaData[$name])) {
+            return '';
+        }
+        return $wpMetaMapper->maybe_unserialize($this->metaData[$name]);
+    }
+
+
 }
