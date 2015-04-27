6ab7e2c3a8a2b7fca0fb808d01321f995098b038
Kathryn Reeve <Kathryn@binaryKitten.com>
2015-04-27 23:21:23 +0100

    added ZFC base to module list

---
+++ public/module/Application/src/Application/Mapper/AbstractMapper.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kat
+ * Date: 27/04/2015
+ * Time: 22:21
+ */
+
+namespace Application\Mapper;
+
+
+class AbstractMapper {
+
+}
---
+++ public/module/Application/src/Application/Mapper/LdapMapper.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kat
+ * Date: 18/04/2015
+ * Time: 21:14
+ */
+
+namespace Application\Mapper;
+
+
+class LdapMapper {
+
+}
