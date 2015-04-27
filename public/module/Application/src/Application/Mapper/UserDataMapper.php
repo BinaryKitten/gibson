1e92bef68291a0525445a8db244ce80fb243141a
Kathryn Reeve <Kathryn@binaryKitten.com>
2015-04-27 23:26:34 +0100

    update to add new mappers for rfid and user data

--- public/module/Application/src/Application/Mapper/UserDataMapper.php
+++ public/module/Application/src/Application/Mapper/UserDataMapper.php
@@ -2,13 +2,70 @@
 
 namespace Application\Mapper;
 
+use Application\Model\WPUser;
+use Zend\Db\Adapter\Driver\ResultInterface as DbResult;
+use Zend\ServiceManager\ServiceLocatorInterface;
 use ZfcBase\Mapper\AbstractDbMapper;
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
 
-class UserDataMapper extends AbstractDbMapper
+class UserDataMapper extends AbstractDbMapper implements ServiceLocatorAwareInterface
 {
     /**
      * @var string
      */
     protected $tableName = 'gibson_user_data';
-    
+
+    /**
+     * @var ServiceLocatorInterface
+     */
+    protected $serviceLocator = null;
+
+
+    /**
+     * Set service locator
+     *
+     * @param ServiceLocatorInterface $serviceLocator
+     */
+    public function setServiceLocator(ServiceLocatorInterface $serviceLocator)
+    {
+        $this->serviceLocator = $serviceLocator;
+    }
+
+    /**
+     * Get service locator
+     *
+     * @return ServiceLocatorInterface
+     */
+    public function getServiceLocator()
+    {
+        return $this->serviceLocator;
+    }
+
+    /**
+     * @param WPUser $wpUser
+     *
+     * @return DbResult
+     */
+    public function createUserDataFromWordpress($wpUser)
+    {
+        /** @var WPUserMetaMapper $wpUserMeta */
+        $wpUserMeta = $this->getServiceLocator()->get('Application\Mapper\WPUserMetaMapper');
+        $metaData = $wpUserMeta->getMetaForUser($wpUser);
+
+        return $this->insert(array(
+           'samAccountName' => $wpUser->user_login,
+            'phone' => '',
+            'emergency_details' => '',
+            'emergency_details_expiry' => '',
+            'medical_information' => '',
+            'medical_information_expiry' => '',
+            'address_line1' => '',
+            'address_line2' => '',
+            'address_town' => '',
+            'address_city' => '',
+            'address_county' => '',
+            'address_postcode' => '',
+        ));
+    }
+
 }
--- public/module/Application/src/Application/Mapper/UserRFIDMapper.php
+++ public/module/Application/src/Application/Mapper/UserRFIDMapper.php
@@ -2,6 +2,10 @@
 
 namespace Application\Mapper;
 
+use Application\Exception\RFIDException;
+use Application\Model\UserData as UserDataModel;
+use Application\Model\WPUser as WPUserModel;
+use Zend\Db\Adapter\Exception\InvalidQueryException;
 use ZfcBase\Mapper\AbstractDbMapper;
 
 class UserRFIDMapper extends AbstractDbMapper
@@ -11,4 +15,36 @@ class UserRFIDMapper extends AbstractDbMapper
      */
     protected $tableName = 'gibson_user_rfid';
 
+    public function addRFIDtoUser($user, $rfid, $description = '')
+    {
+       $samAccountName = $this->getSamAccountNameFromUser($user);
+
+        $data = [
+            'rfidCode' => $rfid,
+            'description' => $description,
+            'samAccountName' => $samAccountName
+        ];
+        try {
+            $result = $this->insert($data);
+        } catch(InvalidQueryException $queryException) {
+            throw RFIDException::getRFIDExceptionFromQueryException($queryException);
+        }
+        \Zend\Debug\Debug::dump($result);
+    }
+
+    protected function getSamAccountNameFromUser($user)
+    {
+        if ($user instanceof UserDataModel || property_exists($user, 'samAccountName')) {
+            $samAccountName = $user->samAccountName;
+        } elseif ($user instanceof WPUserModel || property_exists($user, 'user_login')) {
+            $samAccountName = $user->user_login;
+        } elseif (is_string($user)) {
+            $samAccountName = $user;
+        }
+
+        if (!isset($samAccountName)) {
+            throw new \InvalidArgumentException('Cannot Discern samAccountName from User Object/Data');
+        }
+        return $samAccountName;
+    }
 }
