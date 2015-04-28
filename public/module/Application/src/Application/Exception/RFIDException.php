db04d653886b4323a5b63bc9ebdd315666bc957a
Kathryn Reeve <Kathryn@binaryKitten.com>
2015-04-28 16:42:52 +0100

    add RFID Exception - used in adding RFID to users

--- public/module/Application/src/Application/Exception/RFIDException.php
+++ public/module/Application/src/Application/Exception/RFIDException.php
@@ -9,6 +9,38 @@
 namespace Application\Exception;
 
 
-class RFIDException {
+use Zend\Db\Adapter\Exception\InvalidQueryException;
 
+class RFIDException extends \Exception
+{
+    const RFID_EXISTS_FOR_USER = 1;
+    const RFID_EXISTS_FOR_OTHER_USER = 2;
+
+    const MSG_RFID_EXISTS_FOR_USER = 'The RFID already Exists for this user';
+    const MSG_RFID_EXISTS_FOR_OTHER_USER = 'The RFID already Exists for a user';
+
+    public static function getRFIDExceptionFromQueryException(InvalidQueryException $queryException)
+    {
+        $exception = $queryException->getPrevious();
+        switch ($exception->getCode() . '-' . $exception->errorInfo[1]) {
+            case '23000-1062':
+                if (strpos($queryException->getMessage(), "for key 'PRIMARY'") !== false) {
+                    $newCode = RFIDException::RFID_EXISTS_FOR_USER;
+                    $newMsg = RFIDException::MSG_RFID_EXISTS_FOR_USER;
+                } elseif (strpos($queryException->getMessage(), "for key 'no_rfid_same_across_users'") !== false) {
+                    $newCode = RFIDException::RFID_EXISTS_FOR_OTHER_USER;
+                    $newMsg = RFIDException::MSG_RFID_EXISTS_FOR_OTHER_USER;
+                }
+                return new RFIDException(
+                    $newMsg,
+                    $newCode,
+                    $queryException
+                );
+                break;
+
+            default:
+                return $queryException;
+                break;
+        }
+    }
 }
